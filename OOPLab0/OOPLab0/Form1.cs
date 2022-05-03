181979a70572f760587ad97f97f50cad0e7ee8a4
Ramunis <seva.m@live.com>
2022-05-03 18:35:03 +1000

    OOP App v1.0

---
+++ OOPLab0/OOPLab0/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static PercentLibrary.Calc;
+
+namespace OOPLab0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PercentLibrary.Calc s1 = new PercentLibrary.Calc(0,Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+            label1.Text = Convert.ToString(s1.sum());
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PercentLibrary.Calc test = new PercentLibrary.Calc(0, Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+            test.test(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text), test.sum());
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PercentLibrary.Calc v1 = new PercentLibrary.Calc(Convert.ToDouble(textBox3.Text), 0, Convert.ToDouble(textBox4.Text));
+            label4.Text = Convert.ToString(v1.val());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            PercentLibrary.Calc2 p1 = new PercentLibrary.Calc2(Convert.ToDouble(textBox5.Text), Convert.ToDouble(textBox6.Text),0);
+            label7.Text = Convert.ToString(p1.per());
+
+        }
+
+        private void Override_Click(object sender, EventArgs e)
+        {
+            PercentLibrary.Calc2 test1 = new PercentLibrary.Calc2(0,0,0);
+            test1.test();
+
+        }
+    }
+}
---
+++ PercentLibrary/PercentLibrary/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PercentLibrary
+{
+    public class Calc
+    {
+        protected double S;
+        protected double V;
+        protected double P;
+
+        public Calc(double s,double v,double p)
+        {
+            this.S = s;
+            this.V = v;
+            this.P = p;
+        }
+
+        public double sum()
+        {
+            S = (V * 100) / P;
+            return S;
+        }
+
+        public double val()
+        {
+            V = (S / 100) * P;
+            return V;
+        }
+
+        public double per()
+        {
+            P = (V * 100) / S;
+            return P;
+        }
+
+        public void test(double s,double v,double p)
+        {
+            MessageBox.Show("Class is runnable:"+s+";"+v+";"+p);
+        }
+    }
+
+    public class Calc2 : Calc  // Наследование
+    {
+        public Calc2(double s, double v, double p) : base (s,v,p)  // Наследование
+        {
+            
+        }
+
+        public void setsum(Double s)  //Инкапсуляция
+        {
+            this.S = s;
+        }
+
+        public Double getsum()        //Инкапсуляция
+        {
+            return this.S;
+        }
+
+        public void setval(Double v)  //Инкапсуляция
+        {
+            this.V = v;
+        }
+
+        public Double getval()        //Инкапсуляция
+        {
+            return this.V;
+        }
+
+        public void setper(Double p)  //Инкапсуляция
+        {
+            this.P = p;
+        }
+
+        public Double getper()        //Инкапсуляция
+        {
+            return this.P;
+        }
+
+
+        public void test() // Инкапсуляция
+        {
+            MessageBox.Show("Override");
+        }
+
+    }
+
+
+}
